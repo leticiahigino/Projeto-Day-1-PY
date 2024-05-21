297842672ac0eb061874689f1119b747adec2c7c
Leticia Higino Camargo <115560784+leticiahigino@users.noreply.github.com>
2024-05-21 12:30:09 -0300

    Add files via upload

---
+++ projetos/Day1.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print ("Seja bem vindos ao gerador de nome de bandas!")
+
+#2. Ask the user for the city that they grew up in.
+cidade = input ("Qual o nome da cidade que você nasceu? ")
+
+#3. Ask the user for the name of a pet.
+pet_nome = input ("Qual o nome do seu pet? ")
+
+#4. Combine the name of their city and pet and show them their band name.
+print ("o nome de sua banda é: "+ cidade + " " + pet_nome)
